97fcd07336b88848501747573d0cec8c9ecfc711
rafi1306 <rafipratama29@gmail.com>
2020-09-08 09:25:44 +0700

    menambah user controller dan card list users

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/dashboard', 'dashboard\DashboardController@index');
+// Users
+Route::get('/dashboard/users', 'dashboard\UserController@index');
